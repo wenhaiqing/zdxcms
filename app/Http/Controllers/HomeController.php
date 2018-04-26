e31cca6c89dd66ae77f8d412807645e4c6859c33
wenhaiqing <243083741@qq.com>
2018-04-26 09:10:22 +0800

    test‘ ；

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,11 @@ class HomeController extends Controller
 
     public function test()
     {
-        $qianyi = Qianyi::find(1);
-        $qianyi->user->notify(new MemberQianyi($qianyi));
+        $num = Mail::raw('邮件内容', function($message) {
+
+            $message->from('whqrlm@163.com', '发件人名称');
+            $message->subject('邮件主题');
+            $message->to('243083741@qq.com');
+        });
     }
 }
