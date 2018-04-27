006693f683dc20e92c07286a9041c85b3946b139
wenhaiqing <243083741@qq.com>
2018-04-27 17:41:01 +0800

    修改redis读取流时间

--- config/database.php
+++ config/database.php
@@ -113,6 +113,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 1,
+            'read_write_timeout' => 0,//new
         ],
 
     ],
