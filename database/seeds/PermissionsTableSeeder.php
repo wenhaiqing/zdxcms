ae497b0537ab2569fce8d810b12778609197292d
wenhaiqing <243083741@qq.com>
2018-04-29 18:06:03 +0800

    添加权限

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -524,18 +524,8 @@ class PermissionsTableSeeder extends Seeder
             'updated_at'=>Carbon::now()->toDateTimeString()
         ]);
         $signs = Permission::create([
-            'name' => "manage_signs",
-            'pid' => $dang->id,
-            'icon' => "fa fa-users",
-            'url' => "zdxadmin/signs",
-            'remarks' => "签到管理",
-            'status'=>1,
-            'created_at'=>Carbon::now()->toDateTimeString(),
-            'updated_at'=>Carbon::now()->toDateTimeString()
-        ]);
-         Permission::create([
-            'name' => "edit_signs",
-            'pid' => $signs->id,
+            'name' => "mamage_signs",
+            'pid' => $members->id,
             'icon' => "fa fa-users",
             'url' => "zdxadmin/signs",
             'remarks' => "查看签到",
