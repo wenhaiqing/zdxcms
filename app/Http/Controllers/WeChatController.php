e7124bc5947f157a45f7c7f1edf0eabcde57623d
wenhaiqing <243083741@qq.com>
2018-05-02 13:24:20 +0800

    test

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -119,6 +119,18 @@ class WeChatController extends Controller
 
         $app = Factory::officialAccount($config);
         $oauth = $app->oauth;
+        // 未登录
+        if (empty($_SESSION['wechat_user'])) {
+
+            $_SESSION['target_url'] = '/wap/bind_wechat';
+            \Log::info($_SESSION['target_url']);
+            return $oauth->redirect();
+            // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
+            // $oauth->redirect()->send();
+        }
+
+// 已经登录过
+        $user = $_SESSION['wechat_user'];
 
     }
 
