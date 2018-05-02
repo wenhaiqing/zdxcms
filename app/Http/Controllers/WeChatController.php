2f2c6769933a9ba2f02d5a0d6eae6a203137f4b9
wenhaiqing <243083741@qq.com>
2018-05-02 15:17:49 +0800

    test

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -172,7 +172,7 @@ class WeChatController extends Controller
         }
         $user = $request->session()->get('wechat_user');
         $openid = $user['id'];
-        $member = Member::where('id',\Auth::guard('wap')->id)->update(['openid'=>$openid]);
+        $member = Member::where('id',\Auth::guard('wap')->id())->update(['openid'=>$openid]);
         if ($member){
             flash('微信绑定成功');
             return redirect()->route('wap.center');
