660a1332ba23f1f97e975e390fa1cfe40c37dffd
wenhaiqing <243083741@qq.com>
2018-05-13 16:08:12 +0800

    修改首页

--- resources/views/wap/dang/index.blade.php
+++ resources/views/wap/dang/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <title>首页</title>
+    <title>吕梁智慧党建云</title>
     <link rel="stylesheet" type="text/css" href="{{asset('wap/new/css/index.css')}}"/>
     <link rel="stylesheet" type="text/css" href="{{asset('wap/new/css/lrtk2.css')}}"/>
     <script src="{{asset('wap/llmap/js/jquery-3.3.1.min.js')}}"></script>
