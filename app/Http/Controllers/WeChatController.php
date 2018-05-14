5d4c3417206282cb0ee5b2373f0663ff84c3b4ff
wenhaiqing <243083741@qq.com>
2018-05-14 14:42:56 +0800

    修改绑定微信

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -172,8 +172,9 @@ class WeChatController extends Controller
         }
         $user = $request->session()->get('wechat_user');
         $openid = $user['id'];
-        $member = Member::where('id',\Auth::guard('wap')->id())->update(['openid'=>$openid]);
         Member::where('openid',$openid)->update(['openid'=>'']);
+        $member = Member::where('id',\Auth::guard('wap')->id())->update(['openid'=>$openid]);
+
         if ($member){
             flash('微信绑定成功');
             return redirect()->route('wap.center');
