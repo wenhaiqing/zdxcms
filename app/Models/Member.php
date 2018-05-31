d3e7eb476f4351389f1d7e469e13654f21153aba
wenhaiqing <243083741@qq.com>
2018-05-31 15:14:05 +0800

    修改会员密码加密

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -34,4 +34,15 @@ class Member extends User
         $user = \App\Models\User::where('id',$pid)->first();
         return $user->name;
     }
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
 }
